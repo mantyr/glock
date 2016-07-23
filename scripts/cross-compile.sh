001542bab277b9d2a407d3d095a83f758fec7200
KyleBanks <kyle@nthgensoftware.com>
2016-07-23 07:56:37 -0400

    Remove the docker nonsense from the cross-compilation script

--- scripts/cross-compile.sh
+++ scripts/cross-compile.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# Uses Docker to cross-compile glock for Mac/Windows/Linux with 32 & 64-bit variations of each.
+# Cross-compile glock for Mac/Windows/Linux with 32 & 64-bit variations of each.
 #
 # After compilation, the binaries can be found in the dist/ directory.
 #
@@ -9,7 +9,6 @@
 
 cd $GOPATH/src/github.com/KyleBanks/glock
 
-docker run --rm -it -v "$GOPATH":/go -w /go/src/github.com/KyleBanks/glock golang:1.6 bash
 for GOOS in darwin linux windows; do
     for GOARCH in 386 amd64; do
         go build -v -o dist/glock-$GOOS-$GOARCH
